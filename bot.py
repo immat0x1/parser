e2c6bf82afe3466f919211a06b15cd9babcb82b2
immat0x1 <immat0x1@protonmail.com>
2022-07-04 01:13:37 +0500

    fixes

--- bot.py
+++ bot.py
@@ -4,23 +4,40 @@ from bs4 import BeautifulSoup, Comment
 from aiogram import Bot, Dispatcher, executor, types
 import logging
 import base64
+from ids import ugatu_ids, ugntu_ids
 
 b64 = "NTQxMzA5MTI5NjpBQUhTZlFXNEZDbC11eHM2QnNhRmo2WVFBVlZkSVNzbmNzTQ=="
-bot = Bot(token=base64.b64decode(base64_message.encode('ascii')).decode('ascii'), parse_mode=types.ParseMode.MARKDOWN_V2)
+bot = Bot(token=base64.b64decode(b64.encode('ascii')).decode('ascii'), parse_mode=types.ParseMode.MARKDOWN_V2)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
+
+@dp.message_handler(commands="ugntu")
+async def parseUgntu(message: types.Message):
+    msg = message.text.split()
+    snils = msg[1]
+    url = "https://ams.rusoil.net/abitonline/onlinepeople_list.php"
+    await message.answer("**Started parsing for UGNTU**")
+    for id in ugntu_ids:
+        cnt = 1
+        soup = BeautifulSoup(requests.get(url + "?id=" + id).text, 'html.parser')
+        cells = soup.findAll('tr')
+        for cell in cells:
+    	    if snils in cell.text:
+    	        num = cells[cells.index(cell)].find('td', align="center").text
+    	        await message.answer(f"`{ugntu_ids.get(id)}`" + '\n\n' + f"Место по баллам: {num.replace('. ', '')}" + '\n' + f"Место по зачислениям: {cnt}")
+    	        break
+    	    else:
+    	        if "Согласие на зачисление" in cell.text:
+    	            cnt += 1
 
 @dp.message_handler(commands="ugatu")
 async def parseUgatu(message: types.Message):
     msg = message.text.split()
     snils = msg[1]
     url = "https://www.ugatu.su/abitur/bachelor-and-specialist/admission-ratings"
-    r = requests.get(url)
-    options = BeautifulSoup(r.text, 'html.parser').find('select', id="id_specialty").findAll('option')
-    options.pop(0)
-    ids = sorted(set([k['value'] for k in options]), key=int)
     snils = snils[:3] + "-" + snils[3:6] + "-" + snils[6:9] + " " + snils[9:]
-    for id in ids:
+    await message.answer("**Started parsing for UGATU**")
+    for id in ugatu_ids:
         cnt = 1
         institution = "УГАТУ" # Филиал+в+г.+Ишимбае | Филиал+в+г.+Кумертау | УГАТУ
         funding = "Бюджетная+основа" # Полное+возмещение+затрат | Бюджетная+основа
@@ -34,31 +51,10 @@ async def parseUgatu(message: types.Message):
         for i in cells:
             data = cells[cells.index(i)].findAll('td')
             if snils == data[2].text:
-    	        await message.answer(f"`{BeautifulSoup(r.text, 'html.parser').find('option', value=id).text}`" + '\n\n' + f"Место по баллам: {data[0].text}" + '\n' + f"Место по зачислениям: {cnt}" + '\n' + info[3].text.replace('\n', '').replace('  ', '').replace('.', '\n'))
+    	        await message.answer(f"`{ugatu_ids.get(id)}`" + '\n\n' + f"Место по баллам: {data[0].text}" + '\n' + f"Место по зачислениям: {cnt}" + '\n' + info[3].text.replace('\n', '').replace('  ', '').replace('.', '\n'))
     	        break
             else:
     	        if data[8].text == 'Да':
-    	            cnt += 1
-
-@dp.message_handler(commands="ugntu")
-async def parseUgntu(message: types.Message):
-    msg = message.text.split()
-    snils = msg[1]
-    url = "https://ams.rusoil.net/abitonline/onlinepeople_list.php"
-    comment = BeautifulSoup(requests.get(url).text, 'html.parser').find(text=lambda text:isinstance(text, Comment))
-    options = BeautifulSoup("". join(comment), 'html.parser').findAll('option')
-    ids = sorted(set([k['value'] for k in options]), key=int)
-    for id in ids:
-        cnt = 1
-        soup = BeautifulSoup(requests.get(url + "?id=" + id).text, 'html.parser')
-        cells = soup.findAll('tr')
-        for cell in cells:
-    	    if snils in cell.text:
-    	        num = cells[cells.index(cell)].find('td', align="center").text
-    	        await message.answer(f"`{BeautifulSoup(''. join(comment), 'html.parser').find('option', value=id).text}`" + '\n\n' + f"Место по баллам: {num.replace('. ', '')}" + '\n' + f"Место по зачислениям: {cnt}")
-    	        break
-    	    else:
-    	        if "Согласие на зачисление" in cell.text:
     	            cnt += 1
 
 if __name__ == '__main__':
